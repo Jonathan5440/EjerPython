11737a8cd67451c3998eb288f1ef6c017c8f9646
Jonathan5440 <jvaldezd4@miumg.edu.gt>
2025-10-17 23:42:27 -0600

    primos

---
+++ for_05_primos_1_a_50.py
@@ -0,0 +1,12 @@
+
+def es_primo(x):
+    if x <= 1: return False
+    if x <= 3: return True
+    if x % 2 == 0 or x % 3 == 0: return False
+    i = 5
+    while i*i <= x:
+        if x % i == 0 or x % (i+2) == 0: return False
+        i += 6
+    return True
+for n in range(1,51):
+    if es_primo(n): print(n)
