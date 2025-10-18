933d055f8a110dc28314ba20e8db7e8ae1ad6796
Jonathan5440 <jvaldezd4@miumg.edu.gt>
2025-10-18 04:33:12 -0600

    determinar calificacion

---
+++ if_09_calificacion_letra.py
@@ -0,0 +1,11 @@
+
+try:
+    n = float(input("Calificacion 0-100: "))
+    if n < 0 or n > 100: print("fuera de rango")
+    elif n >= 90: print("A")
+    elif n >= 80: print("B")
+    elif n >= 70: print("C")
+    elif n >= 60: print("D")
+    else: print("F")
+except ValueError:
+    print("Entrada invalida.")
